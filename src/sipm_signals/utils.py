5e55d05469e9ede3e3d29cd3cb1e03088912d48b
Alperen Aksoy <a.aksoy@fz-juelich.de>
2025-10-22 13:01:10 +0200

    moved function skw() to utils

--- src/sipm_signals/utils.py
+++ src/sipm_signals/utils.py
@@ -1,3 +1,4 @@
+from random import random
 import numpy as np
 
 
@@ -6,3 +7,18 @@ def diversity_score(res_g, res_b):
        return np.sum(np.uint8(res_g != res_b)) / 2 / len(res_g)
 
 
+def skw(p: float = 0.2) -> int:
+        """
+        Generate a skewed binary value.
+
+        Parameters
+        ----------
+        p : float, optional
+            Probability of returning 0. Default is 0.2.
+
+        Returns
+        -------
+        int
+            1 with probability (1-p), 0 with probability p.
+        """
+        return 1 if random() > p else 0
