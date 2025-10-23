4f05c464298b4f1949d428a2636b401c2ea6185f
Alperen Aksoy <a.aksoy@fz-juelich.de>
2025-10-23 10:50:16 +0200

    extendet init

--- src/genetic_algorithm/__init__.py
+++ src/genetic_algorithm/__init__.py
@@ -1,8 +1,14 @@
 from .algorithm import GeneticAlgorithm
 from .utils import diversity, time_elapsed
+from .statistics import create_stats
+from .toolbox_utils import create_toolbox
+from .evaluation import NNEvaluator
 
 __all__ = [
     "GeneticAlgorithm",
     "diversity",
     "time_elapsed",
+    "create_stats",
+    "create_toolbox",
+    "NNEvaluator"
 ]
